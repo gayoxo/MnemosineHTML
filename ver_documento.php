3d47eb4dff529adfaa1303a54a5d61e505ba7997
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2018-01-16 12:49:13 +0100

    Final mapas Mnemosine

--- ver_documento.php
+++ ver_documento.php
@@ -96,6 +96,7 @@ function ProcesaLista($ArrayE,$lis,$Ini)
 			{
 				
 				$Posiciones=$Value[0];
+				$ValoresHijos=$Value[1];
 				$InicialPos=$Posiciones[0];
 				echo "<div id=\"map".$Ini."\" class=\"map\"></div>";
 				echo "<script>
@@ -117,25 +118,35 @@ function ProcesaLista($ArrayE,$lis,$Ini)
 		$icon="\"geo/IconoAzul.png\"";
 	
 	$valorMarca =$Posiciones[$i];
+	$valorMarcaHijos =$ValoresHijos[$i];
 	echo "
 	map.addMarker({
   lat: ".$valorMarca[0].",
   lng: ".$valorMarca[1].",
   icon: ".$icon.",
   infoWindow: {
-  content: '<p>HTML Content</p>'
+  content: '<p>";
+  
+  ProcesaLista($valorMarcaHijos,true,$Ini);
+  
+  echo "</p>'
 }
 	});";
 	}
 
-//
-//path = [[-12.044012922866312, -77.02470665341184], [-12.05449279282314, -77.03024273281858], [-12.055122327623378, -77.03039293652341], [-12.075917129727586, -77.02764635449216], [-12.07635776902266, -77.02792530422971], [-12.076819390363665, -77.02893381481931], [-12.088527520066453, -77.0241058385925], [-12.090814532191756, -77.02271108990476]];	
-//	echo "map.drawPolyline({
-//  path: ".$Posiciones.",
-//  strokeColor: '#131540',
-//  strokeOpacity: 0.6,
-//  strokeWeight: 6
-//});";
+$psjason=json_encode($Posiciones);
+
+//echo $psjason;
+$psjason = str_replace('"','', $psjason);
+
+echo "pathe =".$psjason.";";
+// [[-12.044012922866312, -77.02470665341184], [-12.05449279282314, -77.03024273281858], [-12.055122327623378, -77.03039293652341], [-12.075917129727586, -77.02764635449216], [-12.07635776902266, -77.02792530422971], [-12.076819390363665, -77.02893381481931], [-12.088527520066453, -77.0241058385925], [-12.090814532191756, -77.02271108990476]];	";
+	echo "map.drawPolyline({
+  path: pathe,
+  strokeColor: '#db6c5a',
+  strokeOpacity: 0.8,
+  strokeWeight: 6
+});";
 	
 	echo "</script>";
 	$Ini=$Ini+1;
