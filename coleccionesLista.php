fe37d7fa069820c49bba3b66783b2aacb576d33a
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-09-13 12:16:13 +0200

    nueva mnemosine

--- coleccionesLista.php
+++ coleccionesLista.php
@@ -252,7 +252,7 @@ class CollectionArray
 		22,
 		"Autores Extremeños",
 		"Autores Extremeños. Coordinador: Guadalupe Nieto Caballero.",
-		"name=Autores Extremeños&q=coleccionenmnemosine:%22Autores extremeños%22",
+		"name=Autores Extremeños&q=coleccionenmnemosine:%22extremenos%22",
 		"imagenes/COLECCION3.png"
 		),
 
