25215be6085b1c3c7013ed772518ae93f8d9bc3a
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-12-01 12:24:36 +0100

    mejoras rediseño

---
+++ form_buscador_Oculto.php
@@ -0,0 +1,12 @@
+	<div class="buscador_simple_zona buscador_simple">
+	<div>
+        </br>
+        </br>
+        </br>
+        </br>
+	</div>
+	</br>
+	</div>	
+	</br>
+	</br>
+	</div> <!-- Cierre de la cabecera con imagen -->
