b12d2db049d72122b791a21c90899c4583016f82
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-12-10 11:31:39 +0100

    redesign

--- queesmnemosine.php
+++ queesmnemosine.php
@@ -16,12 +16,12 @@
 <i>Mnemosine</i>, Biblioteca Digital de La otra Edad de Plata,reúne un repertorio de textos y autores que han permanecido a la sombra de las grandes figuras literarias del primer tercio del siglo XX. La biblioteca pretende ser un <u>laboratorio</u> en el que se puedan importar, enriquecer, enlazar, visualizar datos y metadatos y ponerlos en abierto con el fin de responder a los retos de la historia cultural y digital de este periodo.
         </p>
 		<div class="imagen_centrada">
-			<img class="icono_entre_parrafos" src="imagenes/red180.png" width="170" height="170" alt="">
+			<img class="icono_entre_parrafos" src="imagenes/red290.png" width="170" height="170" alt="">
 		</div>
 		<p class="justifiedtext">
 		Además, <i>Mnemosine</i>, como <a class="justifiedtext_link" href="http://mysmartlibrary.es">smartlibrary</a>, pretende ser un <u>campo de experimentación internacional</u> para la creación de herramientas y de redes semánticas interoperables a través de las cuales se genere investigación e innovación de calidad y nuevos modelos teóricos de interpretación de textos literarios.		</p>
 		<div class="imagen_centrada">
-			<img class="icono_entre_parrafos" src="imagenes/red90.png" width="170" height="170" alt="">
+			<img class="icono_entre_parrafos" src="imagenes/red2.png" width="170" height="170" alt="">
 		</div>
 		<p class="justifiedtext">
 
@@ -29,8 +29,7 @@
             el <a class="justifiedtext_link" href="https://portal.mineco.gob.es/es-es/">Ministerio de Economía y Competitividad</a> con el proyecto de investigación: <a class="justifiedtext_link" href="https://www.ucm.es/escritorios-electronicos-literaturas-2/">“Escritorios Electrónicos para las Literaturas-2”(FFI2012-34666 , 2013-2015)</a> y por la <a class="justifiedtext_link" href="https://www.fbbva.es/">Fundación BBVA</a> con el proyecto de investigación “Modelo Unificado de Gestión de Colecciones Digitales con Estructuras Reconfigurables: Aplicación a la Creación de Bibliotecas Digitales Especializadas para Investigación y Docencia” (HUM14_251, 2015-2016). Los libros interactivos se crearon gracias a la colaboración de Biblioteca Nacional de España y a la  financiación del proyecto <a class="justifiedtext_link" href="https://www.ucm.es/edicionliterariaelectronica">“Edición Literaria Electrónica”</a>(eLItE-CM, H215/HUM-34-26, 2016- 2018).
         </p>
         <p class="justifiedtext">
-            La actualización de los contenidos de Mnemosine ha sido posible gracias a los proyectos
-            “Creación, exploración y transformación de repositorios de objetos educativos en
+            La actualización de los contenidos de <i>Mnemosine</i> ha sido posible gracias a los proyectos “Creación, exploración y transformación de repositorios de objetos educativos en
             dominios especializados” (TIN2017-88092-R, 2018-2021) y <a class="justifiedtext_link" href="https://www.ucm.es/proyectomnemosine/">“Mnemosine: hacia la historia digital de la otra Edad de Plata: producción, almacenamiento, uso y difusión”</a> (RTI2018-095522-B-I00, 2019-2022)
             financiados por la <a class="justifiedtext_link" href="http://www.aei.gob.es/">Agencia Estatal de Investigación</a>.
 		</p>
