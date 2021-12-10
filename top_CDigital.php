65cd3ae217600094e6299e3a976eb0449a2c54f4
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-12-10 12:20:48 +0100

    redesign

---
+++ top_CDigital.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta name="keywords" content="Biblioteca,UCM,ILSA,LOEP,Edad,Plata">
+	<meta name="description" content="Biblioteca Mnemosine, Datos de autores y obras que pertenecen al repertorio de raros y olvidados (1868-1939)">
+	<meta name="author" content="Joaquin Gayoso-Cabada">
+	<meta name="author" content="Dolores Romero">
+    <title>Biblioteca Digital Mnemosine</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="responsive-nav.css">
+	<script src="responsive-nav.js"></script>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="nhpup_1.1.js"></script>
+	<script src="wordcloud2.js"></script>
+	  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPj8iz7libsW74GvKYCU7VdtggaOA8814&v=3&sensor=true&libraries=places"></script>  	
+
+  <script src="gmaps.js"></script>
+  <style type="text/css">
+    .map {
+      width: 100%;
+      height: 400px;
+    }
+  </style>
+  
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  
+  </head>
+  <body>
+  
+  
+  <div class="menu_div" id="nav">
+   <nav class="nav-collapse menu_propio">
+			<ul class="menu_list">
+				<li class="li_menunavicon li_menunav">
+				<a href="index.php"> 
+				<img class="iconomenu menu_elem Bannericon" src="imagenes/logomenu2.png" width="100" height="100" alt=""> 
+				</a>
+				</li>
+				<li class="li_menunav"><a class="menu_elem" href="queesmnemosine.php">¿Qué es Mnemosine?&nbsp></a></li>
+				<li class="li_menunav"><a class="menu_elem" href="colecciones.php">Colecciones&nbsp></a></li>
+                <li class="li_menunav"><a class="menu_elem" href="stadisticasTest.php">Visualizaciones&nbsp></a></li>
+                <li class="li_menunav"><a class="menu_elem" href="equipodeinvestigacion.php">Grupos, equipo y contacto&nbsp></a></li>
+				<li class="li_menunav"><a class="menu_elem" href="ayuda.php">Ayuda&nbsp></a></li>
+                <li class="li_menunav"><a class="menu_elem" href="">Cómo Citar&nbsp></a></li>
+            </ul>
+		</nav>
+	</div>
+  
+  <div class="wrapper">
+	<div class="zona_icono_cd">
+	
+	<?php 
+	header('Content-type: text/html; charset=utf-8');
+	?>
+
