546c103f123727dbe71fc67cabf6d68ae9f4e1a5
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-12-10 12:48:00 +0100

    redesign

--- equipodeinvestigacion.php
+++ equipodeinvestigacion.php
@@ -19,6 +19,18 @@
 				</p>
 			</div>
 		</div>
+        <div class="bloque">
+            <div class="imagen_bloque">
+                <a class="quienes_somos_link" href=" https://www.ucm.es/loep" target="_blank">
+                    <img class="imagen_bloque_base" src="imagenes/loep.jpg" alt="">
+                </a>
+            </div>
+            <div class="texto_bloque">
+                <p class="texto-bloque_cuerpo">
+                    <a class="quienes_somos_link" href=" https://www.ucm.es/loep" target="_blank">LOEP</a> (La otra Edad de Plata) se constituyó como Grupo de Investigación UCM en el año 2008 (Registro: 941375), con la necesidad de  atender a aquella literatura de las primeras décadas del siglo XX que había permanecido oscurecida por la brillantez de los grandes nombres y movimientos de la llamada Edad de Plata. Su objetivo es recuperar temas, géneros y autores que fueron cayendo en el olvido, —injusto en la mayoría de los casos—, al quedar excluidos del canon de la época.
+                </p>
+            </div>
+        </div>
 		<div class="bloque">
 			<div class="imagen_bloque">
 				<a class="quienes_somos_link" href="http://ilsa.fdi.ucm.es/" target="_blank">
@@ -39,19 +51,7 @@
 			</div>
 			<div class="texto_bloque">
 				<p class="texto-bloque_cuerpo">
-				<a class="quienes_somos_link" href="https://www.ucm.es/leethi" target="_blank">LEETHI</a> (Literaturas Españolas y Europeas del Texto al Hipermedia) es un Grupo de Investigación nacido en 2000 y reconocido como consolidado por la UCM desde 2005 (Registro: 930262). De carácter interdisciplinar, reúne profesores de las Facultades de Filología y Educación de la UCM especializados en diferentes lenguas y sus literaturas (español, inglés, francés, alemán y neerlandés). Llevados todos por su formación en el ámbito nacional e internacional, la actividad de los miembros del grupo se ha centrado en los cambios de paradigma para el estudio del hecho literario que están produciendo en nuestros días la internacionalización y la globalización, las tecnologías digitales y la creación del EEES, de forma que su investigación ha integrado en todo momento y de forma indisoluble la reflexión literaria, tecnológica y didáctica en torno a dos conceptos: transliteratura e hipermedia.				
-				</p>
-			</div>
-		</div>
-		<div class="bloque">
-			<div class="imagen_bloque">
-				<a class="quienes_somos_link" href=" https://www.ucm.es/loep" target="_blank">
-					<img class="imagen_bloque_base" src="imagenes/loep.jpg" alt="">
-				</a>
-			</div>
-			<div class="texto_bloque">
-				<p class="texto-bloque_cuerpo">
-				<a class="quienes_somos_link" href=" https://www.ucm.es/loep" target="_blank">LOEP</a> (La otra Edad de Plata) se constituyó como Grupo de Investigación UCM en el año 2008 (Registro: 941375), con la necesidad de  atender a aquella literatura de las primeras décadas del siglo XX que había permanecido oscurecida por la brillantez de los grandes nombres y movimientos de la llamada Edad de Plata. Su objetivo es recuperar temas, géneros y autores que fueron cayendo en el olvido, —injusto en la mayoría de los casos—, al quedar excluidos del canon de la época.				
+				<a class="quienes_somos_link" href="https://www.ucm.es/leethi" target="_blank">LEETHI</a> (Literaturas Españolas y Europeas del Texto al Hipermedia) es un Grupo de Investigación nacido en 2000 y reconocido como consolidado por la UCM desde 2005 (Registro: 930262). De carácter interdisciplinar, reúne profesores de las Facultades de Filología y Educación de la UCM especializados en diferentes lenguas y sus literaturas (español, inglés, francés, alemán y neerlandés). S investigación ha integrado en todo momento y de forma indisoluble la reflexión literaria, tecnológica y didáctica en torno a dos conceptos: transliteratura e hipermedia.
 				</p>
 			</div>
 		</div>
@@ -59,7 +59,7 @@
 		<div class="bloque">
 			<div class="imagen_bloque">
 				<a class="quienes_somos_link" href="quienessomos.php">
-					<img class="imagen_bloque_base" src="imagenes/red3.png" alt="">
+					<img class="imagen_bloque_base" src="imagenes/red2.png" alt="">
 				</a>
 			</div>
 			<div class="texto_bloque">
@@ -69,7 +69,7 @@
 					</a>
 				</p>
 				<p class="texto-bloque_cuerpo">
-				Mnemosine es fruto de la colaboración entre especialistas de distintas disciplinas:<br>
+                    La gestión web de los contenidos en <i>Mnemosine</i> ha sido desarrollada por:<br>
 >&nbsp <a class="quienes_somos_link" href="#">José Luis Bueren Gómez-Acebo</a><br>
 >&nbsp <a class="quienes_somos_link" href="http://ilsa.fdi.ucm.es/ilsa/ilsa.php?show=6" target="_blank">Joaquín Gayoso Cabada</a><br>
 >&nbsp <a class="quienes_somos_link" href="http://media.wix.com/ugd/a91d96_d91bef286bb3451a9a1981c5767e6e85.pdf" target="_blank">José Miguel González Soriano</a> <br>
