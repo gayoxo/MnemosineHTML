1f7a854d3e2c26eedc1f5982f3ff86a662581332
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2021-12-28 18:42:57 +0100

    pequeñas mejoras de las descricciones.

--- CartografiaDigital.php
+++ CartografiaDigital.php
@@ -16,35 +16,35 @@
 
         <img src="imagenes/pruebamatrizFINALbaja.png" alt="" width="1280" height="727" usemap="#Map"/>
         <map name="Map">
-            <area shape="poly" coords="347,135,237,130,218,148,240,172,341,172" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Muertos en conflicto (1936-1939)&q=fechademuerte:/193[6789]/&idc=cc_14" target="_blank">
+            <area shape="poly" coords="347,135,237,130,218,148,240,172,341,172" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Muertos en conflicto (1936-1939)&q=fechademuerte:/193[6789]/&idc=cc_14" target="_blank">
             <area shape="poly" coords="795,391" href="#">
-            <area shape="poly" coords="81,339,217,343,197,375,91,379" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Memorias%20de%20Exiliadas&q=subcoleccion:exiliadas&idc=cc_16" target="_blank">
-            <area shape="poly" coords="395,472,476,474,474,498,399,497" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Cuentos%20de%20Calleja&q=subcoleccion:calleja&idc=cc_6" target="_blank">
-            <area shape="poly" coords="148,582,273,571,262,618,164,630" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Carmen%20de%20Burgos&q=REL:62598&idc=cc_19" target="_self">
-            <area shape="poly" coords="57,507,77,534,141,500,196,507,231,536,254,555,268,569,275,556,254,518,204,486,160,478,113,479" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Escritoras&q=genero:mujer" target="_blank">
-            <area shape="circle" coords="237,281,36" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Autores%20en%20exilio&q=exilio:S?&idc=cc_15" target="_blank">
-            <area shape="poly" coords="531,176,530,214,484,216,458,251,430,230,470,184" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Madrid%20en%20la%20Literatura%20de%20la%20Edad%20de%20Plata&q=lugardepublicacion:madrid&idc=cc_17" target="_blank">
-            <area shape="rect" coords="555,232,756,278" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Bohemia%20Literaria&q=subcoleccion:%27Bohemia%20Literaria%27&idc=cc_13" target="_blank">
-            <area shape="rect" coords="611,174,751,219" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Protociencia-ficci%C3%B3n&q=materia:protociencia%20AND%20materia:Ficci?n&idc=cc_8" target="_blank">
-            <area shape="rect" coords="800,122,1058,162" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Instituto Iberoamericano de Berlín&q=localizaciondelfondo:Berlin&idc=cc_12" target="_blank">
-            <area shape="poly" coords="1045,435,1054,462,1007,489,957,542,939,600,925,627,895,611,914,539,977,461" href="http://repositorios.fdi.ucm.es/mnemosine_red/histoANDFilo.php" target="_blank">
+            <area shape="poly" coords="81,339,217,343,197,375,91,379" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Memorias%20de%20Exiliadas&q=subcoleccion:exiliadas&idc=cc_16" target="_blank">
+            <area shape="poly" coords="395,472,476,474,474,498,399,497" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Cuentos%20de%20Calleja&q=subcoleccion:calleja&idc=cc_6" target="_blank">
+            <area shape="poly" coords="148,582,273,571,262,618,164,630" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Carmen%20de%20Burgos&q=REL:62598&idc=cc_19" target="_self">
+            <area shape="poly" coords="57,507,77,534,141,500,196,507,231,536,254,555,268,569,275,556,254,518,204,486,160,478,113,479" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Escritoras&q=genero:mujer" target="_blank">
+            <area shape="circle" coords="237,281,36" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Autores%20en%20exilio&q=exilio:S?&idc=cc_15" target="_blank">
+            <area shape="poly" coords="531,176,530,214,484,216,458,251,430,230,470,184" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Madrid%20en%20la%20Literatura%20de%20la%20Edad%20de%20Plata&q=lugardepublicacion:madrid&idc=cc_17" target="_blank">
+            <area shape="rect" coords="555,232,756,278" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Bohemia%20Literaria&q=subcoleccion:%27Bohemia%20Literaria%27&idc=cc_13" target="_blank">
+            <area shape="rect" coords="611,174,751,219" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Protociencia-ficci%C3%B3n&q=materia:protociencia%20AND%20materia:Ficci?n&idc=cc_8" target="_blank">
+            <area shape="rect" coords="800,122,1058,162" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Instituto Iberoamericano de Berlín&q=localizaciondelfondo:Berlin&idc=cc_12" target="_blank">
+            <area shape="poly" coords="1045,435,1054,462,1007,489,957,542,939,600,925,627,895,611,914,539,977,461" href="http://repositorios.fdi.ucm.es/mnemosine/histoANDFilo.php" target="_blank">
             <area shape="circle" coords="409,656,37" href="https://www.ucm.es/loep" target="_self">
-            <area shape="poly" coords="307,512,279,518,273,451,306,418,364,397,402,390,452,414,473,442,461,453,404,428,343,432,312,467" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Literatura%20de%20Quiosco&q=tipodecoleccion:quiosco&idc=cc_9" target="_blank">
-            <area shape="poly" coords="679,604,809,595,811,619,695,636" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Luis%20Bello&q=REL:61030&idc=cc_20" target="_self">
+            <area shape="poly" coords="307,512,279,518,273,451,306,418,364,397,402,390,452,414,473,442,461,453,404,428,343,432,312,467" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Literatura%20de%20Quiosco&q=tipodecoleccion:quiosco&idc=cc_9" target="_blank">
+            <area shape="poly" coords="679,604,809,595,811,619,695,636" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Luis%20Bello&q=REL:61030&idc=cc_20" target="_self">
             <area shape="poly" coords="1223,88,1237,125,1095,185,1080,162" href="https://biblioteca.ucm.es/hathitrust" target="_blank">
             <area shape="circle" coords="1102,314,70" href="http://www.bne.es/es/Colecciones/LibrosInteractivos/Subcolecciones/edad-plata-interactivo.html" target="_blank">
-            <area shape="poly" coords="745,118,774,117,793,205,822,275,920,375,911,427,830,336,777,256" href="http://repositorios.fdi.ucm.es/mnemosine_red/buscadorColeccion.php?name=Libros%20Digitalizados&q=link:%20/.*/" target="_blank">
+            <area shape="poly" coords="745,118,774,117,793,205,822,275,920,375,911,427,830,336,777,256" href="http://repositorios.fdi.ucm.es/mnemosine/buscadorColeccion.php?name=Libros%20Digitalizados&q=link:%20/.*/" target="_blank">
             <area shape="rect" coords="884,239,1032,285" href="http://www.residencia.csic.es/doc/catalogos.htm" target="_blank">
             <area shape="rect" coords="906,171,1070,215" href="https://www.archivodigitalvalleinclan.es/publica/principal.htm" target="_blank">
             <area shape="poly" coords="1190,179,1202,217,1055,243,1062,211" href=" https://oac.cdlib.org/findaid/ark:/13030/kt9t1nf91c/" target="_blank">
             <area shape="poly" coords="518,453,650,488,620,524,523,501" href="http://repositorios.fdi.ucm.es/mnemosine/ver_documento.php?documento=111845" target="_blank">
             <area shape="poly" coords="466,532,462,579,575,590,584,542" href="http://repositorios.fdi.ucm.es/mnemosine/ver_documento.php?documento=59596" target="_self">
-            <area shape="poly" coords="429,591,541,598,543,648,441,633,429,617" href="http://repositorios.fdi.ucm.es/mnemosine_red/histoANDFilo.php" target="_self">
+            <area shape="poly" coords="429,591,541,598,543,648,441,633,429,617" href="http://repositorios.fdi.ucm.es/mnemosine/histoANDFilo.php" target="_self">
             <area shape="rect" coords="1003,390,1218,439" href="http://www.cervantesvirtual.com/portales/raros_y_olvidados/" target="_blank">
-            <area shape="poly" coords="377,317,397,348,352,387,278,424,270,403" href="http://repositorios.fdi.ucm.es/mnemosine_red/constelacionesCulturales.php" target="_self">
-            <area shape="poly" coords="296,234,288,264,461,346,476,309" href="http://repositorios.fdi.ucm.es/mnemosine_red/stadisticasTest.php" target="_blank">
-            <area shape="poly" coords="1142,560,1107,582,1052,478,1078,458" href="http://repositorios.fdi.ucm.es/mnemosine_red/histoANDFilo.php" target="_blank">
-            <area shape="poly" coords="974,557,963,590,1062,610,1069,583" href="http://repositorios.fdi.ucm.es/mnemosine_red/histoANDFilo.php" target="_blank">
+            <area shape="poly" coords="377,317,397,348,352,387,278,424,270,403" href="http://repositorios.fdi.ucm.es/mnemosine/constelacionesCulturales.php" target="_self">
+            <area shape="poly" coords="296,234,288,264,461,346,476,309" href="http://repositorios.fdi.ucm.es/mnemosine/stadisticasTest.php" target="_blank">
+            <area shape="poly" coords="1142,560,1107,582,1052,478,1078,458" href="http://repositorios.fdi.ucm.es/mnemosine/histoANDFilo.php" target="_blank">
+            <area shape="poly" coords="974,557,963,590,1062,610,1069,583" href="http://repositorios.fdi.ucm.es/mnemosine/histoANDFilo.php" target="_blank">
         </map>
 
 
