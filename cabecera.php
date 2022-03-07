76abd6568f8e4ca28409b17d7df4d0f1cba840cb
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2022-03-07 12:36:18 +0100

    DHAward

--- cabecera.php
+++ cabecera.php
@@ -1,6 +1,7 @@
   <div class="cabecera">
 	<div class="cabecera_smart_library">
-		<img class="cabecera_smart_library_image" src="imagenes/versionbeta2.png" border="0" alt="smart_library_icon">
+        <!--	<img class="cabecera_smart_library_image" src="imagenes/versionbeta2.png" border="0" alt="smart_library_icon"> -->
+        	<img class="cabecera_smart_library_image" src="imagenes/DHAwards.png" border="0" alt="smart_library_icon">
 	</div>
 	  
 	<a href="<?php echo $LinkAtras; ?>"> 
