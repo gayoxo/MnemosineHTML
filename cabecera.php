117437af3d70be933bff08100e19fb29b771aae5
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2022-07-29 11:16:28 +0200

    Cabecera premio

--- cabecera.php
+++ cabecera.php
@@ -1,8 +1,7 @@
   <div class="cabecera">
 	<div class="cabecera_smart_library">
-        <!--	<img class="cabecera_smart_library_image" src="imagenes/versionbeta2.png" border="0" alt="smart_library_icon"> -->
-        	<a href="https://docs.google.com/forms/d/e/1FAIpQLSdPup-iiMrhSyuJtY1PaUv_KoyNkOSzVZcoO9ZWhPkBVvJyvQ/viewform" target="_blank"> <img class="cabecera_smart_library_image" src="imagenes/DHAwards.png" border="0" alt="smart_library_icon"> </a>
-	</div>
+		<img class="cabecera_smart_library_image" src="imagenes/DHPremio.png" border="0" alt="smart_library_icon">
+    </div>
 	  
 	<a href="<?php echo $LinkAtras; ?>"> 
 			<img src="imagenes/Mnemosine2floral.png" width="200" height="200" class="Bannericon" alt="">
